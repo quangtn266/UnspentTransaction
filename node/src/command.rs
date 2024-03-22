720651fdfe639a9351741bd0260c8c25f4e07fdb
quangtn266 <quangtrandn93@gmail.com>
2024-03-22 11:12:17 +0700

    Update source code for lib command and rpc sections

---
+++ node/src/command.rs
@@ -0,0 +1,9 @@
+use crate::{chain_spec, service};
+use crate::{Cli, Subcommand};
+use sc_cli::{SubstrateCli, RuntimeVersion, Role, ChainSpec};
+use sc_service::PartialComponents;
+use node_template_runtime::Block;
+
+impl SubstrateCli for Cli {
+
+}
