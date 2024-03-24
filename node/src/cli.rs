27173169362dbf7ae02b9c9900944f03deac9b2e
quangtn266 <quangtrandn93@gmail.com>
2024-03-24 10:12:30 +0700

    Update source code for src section

---
+++ node/src/cli.rs
@@ -0,0 +1,42 @@
+use structopt::StructOpt;
+use sc_cli::RunCmd;
+
+#[derive(Debug, StructOpt)]
+pub struct Cli {
+    #[structopt(subcommand)]
+    pub subcommand: Option<Subcommand>,
+
+    #[structopt(flatten)]
+    pub run: RunCmd,
+}
+
+#[derive(Debug, StructOpt)]
+pub enum Subcommand {
+    // key management clit utilites
+    Key(sc_cli::KeySubcommand),
+
+    // build a chain specification
+    BuildSpec(sc_cli::BuildSpecCmd),
+
+    // Validate blocks.
+    CheckBlock(sc_cli::CheckBlockCmd),
+
+    // Export blocks.
+    ExportBlocks(sc_cli::ExportBlocksCmd),
+
+    // Export the state of a given block into a chain spec
+    ExportState(sc_cli::ExportStateCmd),
+
+    // Import blocks
+    ImportBlocks(sc_cli::ImportBlocksCmd),
+
+    // Remove the whole chain
+    PurgeChain(sc_cli::PurgeChainCmd),
+
+    // Revert the chain to a previous state
+    Revert(sc_cli::RevertCmd),
+
+    // The custom benchmark subcommand benchmarking runtime pallets.
+    #[structopt(name = "benchmark", about = "benchmark runtime pallets.")]
+    Benchmark(frame_benchmarking_cli::BenchmarkCmd),
+}
